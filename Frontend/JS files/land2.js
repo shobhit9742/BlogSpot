e68f6bbde4ed3a4ce0b9927eba8fd6cd8d569a48
Sagar Prasad <sagar.11704088@gmail.com>
2024-03-28 12:28:38 +0530

    infinte scroll and basic layout created

--- Frontend/JS files/land2.js
+++ Frontend/JS files/land2.js
@@ -1,15 +1,5 @@
 let posts = document.getElementById("card_container");
 
-let fetchData = async () => {
-  try {
-    let res = await fetch("../dummy/dummy.json");
-    let data = await res.json();
-    appendData(data.blog_posts);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 let appendData = (data) => {
   data.forEach((el) => {
     cards(el);
@@ -18,21 +8,68 @@ let appendData = (data) => {
 
 let cards = (data) => {
   const {
-    id,
-    user_profile_image,
-    username,
-    post_creation_datetime,
+    author,
+    dates,
     blog_title,
-    brief_content_summary,
-    tag,
-    estimated_reading_duration,
-    blog_image,
+    blog_links,
+    description,
+    tags,
+    referal,
+    readability_time,
+    blog_img,
   } = data;
 
   // Create card element
   let card = document.createElement("div");
+  card.className = "card";
+
+  let img = document.createElement("img");
+  img.src = blog_img;
+
+  let p = document.createElement("p");
+  p.textContent = author;
+
+  let h3 = document.createElement("h3");
+  h3.textContent = blog_title;
+
+  let desc = document.createElement("p");
+  desc.textContent = description;
+
+  let ref = document.createElement("p");
+  ref.textContent = tags;
+
+  card.append(img, p, h3, desc, ref);
+  posts.append(card);
 };
 
 window.onload = () => {
   fetchData();
 };
+
+async function loadData(numImages = 10) {
+  let i = 0;
+  while (i < numImages) {
+    try {
+      let res = await fetch("../dummy/dummy.json");
+      let data = await res.json();
+      console.log(data);
+      appendData(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+    i++;
+  }
+}
+
+loadData();
+
+window.addEventListener("scroll", () => {
+  console.log("scrolled", window.scrollY);
+  console.log(window.innerHeight);
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    loadData();
+  }
+});
