a0e078b74e04669788aeaf9f592d6694dc041013
Sagar Prasad <sagar.11704088@gmail.com>
2024-03-28 22:01:01 +0530

    spinner + ...
    spinner + card data + sidebar + signIn and signUp modals

--- Frontend/JS files/land2.js
+++ Frontend/JS files/land2.js
@@ -1,10 +1,48 @@
-let posts = document.getElementById("card_container");
+// SignIn Form Details
+let signInEmail = document.getElementById("user_email");
+let signInPass = document.getElementById("user_pwd");
+let signInSubmit = document.getElementById("submit_btn");
+
+signInSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(signInEmail.value);
+  console.log(signInPass.value);
+});
+
+// SignUp Form Details
+let signUpFName = document.getElementById("fName");
+let signUpLName = document.getElementById("lName");
+let signUpEmail = document.getElementById("signUp_email");
+let signUpPass = document.getElementById("signUp_pwd");
+let signUpSubmit = document.getElementById("signUp_submit_btn");
+
+signUpSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(signUpFName.value);
+  console.log(signUpLName.value);
+  console.log(signUpEmail.value);
+  console.log(signUpPass.value);
+});
+
+// Fetch and populate articles
+let url = "https://tech-tatva-2345-1.onrender.com/blog_posts";
+let pageNum = 1;
+let isLoading = false;
+let container = document.querySelector("#container");
+let resultContainer = document.querySelector(".card_section");
+let loadingContainer = document.querySelector(".loading-container");
 
 let fetchData = async () => {
   try {
-    let res = await fetch("../dummy/dummy.json");
+    if (isLoading) return;
+    isLoading = true;
+    loadingContainer.innerHTML = `<div class="spinner-border" role="status">
+                                      <span class="visually-hidden">Loading...</span>
+                                  </div>`;
+
+    let res = await fetch(url);
     let data = await res.json();
-    appendData(data.blog_posts);
+    appendData(data);
   } catch (error) {
     console.log(error);
   }
@@ -12,27 +50,36 @@ let fetchData = async () => {
 
 let appendData = (data) => {
   data.forEach((el) => {
-    cards(el);
+    console.log(el);
+    const { name_title, author_name, profile_Img } = el;
+    card(name_title, author_name, profile_Img);
   });
 };
 
-let cards = (data) => {
-  const {
-    id,
-    user_profile_image,
-    username,
-    post_creation_datetime,
-    blog_title,
-    brief_content_summary,
-    tag,
-    estimated_reading_duration,
-    blog_image,
-  } = data;
-
-  // Create card element
+let card = (name_title, author_name, profile_Img) => {
   let card = document.createElement("div");
+  let title = document.createElement("h1");
+  let author = document.createElement("p");
+  let profile_img = document.createElement("img");
+
+  title.textContent = name_title;
+  author.textContent = author_name;
+  profile_img.src = profile_Img;
+
+  card.append(title, author, profile_img);
+  resultContainer.append(card);
 };
 
-window.onload = () => {
-  fetchData();
+container.onscroll = () => {
+  if (isLoading) return;
+
+  if (
+    Math.ceil(container.clientHeight + container.scrollTop) >=
+    container.scrollHeight
+  ) {
+    pageNum++;
+    fetchData();
+  }
 };
+
+fetchData();
